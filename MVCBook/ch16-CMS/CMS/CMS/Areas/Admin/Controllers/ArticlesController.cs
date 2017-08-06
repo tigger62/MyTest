6087997bcb39cab9dcfd20e547d8a31e573c91cf
Hsu Ting Hua <csu70211122@gmail.com>
2017-08-07 01:21:26 +0800

    練習ASP.NET MVC 書本上第十六章CMS - Day one

---
+++ MVCBook/ch16-CMS/CMS/CMS/Areas/Admin/Controllers/ArticlesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CMS.Models;
+
+namespace CMS.Areas.Admin.Controllers
+{
+    public class ArticlesController : Controller
+    {
+        private CMSDatabaseEntities db = new CMSDatabaseEntities();
+
+        // GET: Admin/Articles
+        public ActionResult Index()
+        {
+            return View(db.Article.ToList());
+        }
+
+        // GET: Admin/Articles/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Article article = db.Article.Find(id);
+            if (article == null)
+            {
+                return HttpNotFound();
+            }
+            return View(article);
+        }
+
+        // GET: Admin/Articles/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Articles/Create
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Subject,Summary,ContentText,IsPublich,PublishDate,ViewCount,CreateUser,CreateDate,UpdateUser,UpdateDate")] Article article)
+        {
+            if (ModelState.IsValid)
+            {
+                article.ID = Guid.NewGuid();
+                db.Article.Add(article);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(article);
+        }
+
+        // GET: Admin/Articles/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Article article = db.Article.Find(id);
+            if (article == null)
+            {
+                return HttpNotFound();
+            }
+            return View(article);
+        }
+
+        // POST: Admin/Articles/Edit/5
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Subject,Summary,ContentText,IsPublich,PublishDate,ViewCount,CreateUser,CreateDate,UpdateUser,UpdateDate")] Article article)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(article).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(article);
+        }
+
+        // GET: Admin/Articles/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Article article = db.Article.Find(id);
+            if (article == null)
+            {
+                return HttpNotFound();
+            }
+            return View(article);
+        }
+
+        // POST: Admin/Articles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            Article article = db.Article.Find(id);
+            db.Article.Remove(article);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MVCBook/ch16-CMS/CMS/CMS/Models/ArticleMetaData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace CMS.Models
+{
+    [System.ComponentModel.DataAnnotations.MetadataType(typeof(ArticleMetaData))]
+    public partial class Article
+    {
+        public class ArticleMetaData
+        {
+            public System.Guid ID { get; set; }
+            [DisplayName("主題")]
+            public string Subject { get; set; }
+            [DisplayName("簡介")]
+            public string Summary { get; set; }
+            [DisplayName("內容")]
+            public string ContentText { get; set; }
+            [DisplayName("是否發佈")]
+            public bool IsPublich { get; set; }
+            [DisplayName("發佈日期")]
+            public System.DateTime PublishDate { get; set; }
+            [DisplayName("瀏灠次數")]
+            public int ViewCount { get; set; }
+            [DisplayName("建立者")]
+            public System.Guid CreateUser { get; set; }
+            [DisplayName("建立時間")]
+            public System.DateTime CreateDate { get; set; }
+            [DisplayName("修改者")]
+            public Nullable<System.Guid> UpdateUser { get; set; }
+            [DisplayName("修改時間")]
+            public System.DateTime UpdateDate { get; set; }
+        }
+    }
+
+}
