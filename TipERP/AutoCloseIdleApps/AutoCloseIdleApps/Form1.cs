196df379369b83f1b92c2c955a81a0e4a26a6524
Hsu Ting Hua <b050404@ccm3s.com>
2018-03-01 18:35:37 +0800

    init project

---
+++ TipERP/AutoCloseIdleApps/AutoCloseIdleApps/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace AutoCloseIdleApps
+{
+    public partial class Form1 : Form
+    {
+        public int Idle_time = 0;
+        public enum MonitorState : int
+        {
+            MONITOR_ON = -1,
+            MONITOR_OFF = 2,
+            MONITOR_STANBY = 1
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// 調用windows API獲取鼠標鍵盤空閒時間
+        /// </summary>
+        /// <param name="plii"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll")]
+        private static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
+        /// <summary>
+        /// 獲取鼠標鍵盤空閒時間
+        /// </summary>
+        /// <returns></returns>
+        public static long GetIdleTick()
+        {
+            LASTINPUTINFO lastInputInfo = new LASTINPUTINFO();
+            lastInputInfo.cbSize = Marshal.SizeOf(lastInputInfo);
+            if (!GetLastInputInfo(ref lastInputInfo)) return 0;
+            return Environment.TickCount - (long)lastInputInfo.dwTime;
+        }
+        [StructLayout(LayoutKind.Sequential)]
+        private struct LASTINPUTINFO
+        {
+            [MarshalAs(UnmanagedType.U4)]
+            public int cbSize;
+            [MarshalAs(UnmanagedType.U4)]
+            public uint dwTime;
+        }
+        public int WM_SYSCOMMAND = 0x0112;
+        public int SC_MONITORPOWER = 0xF170;
+        [DllImport("user32.dll")]
+        private static extern int SendMessage(int hWnd, int hMsg, int wParam, int lParam);
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (GetIdleTick() / 1000 >= Idle_time)//閒置??秒
+            {
+                SendMessage(this.Handle.ToInt32(), WM_SYSCOMMAND, SC_MONITORPOWER, (int)MonitorState.MONITOR_OFF);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int.TryParse(textBox1.Text, out Idle_time);
+                timer1.Interval = Idle_time * 1000;//設定計時器幾毫秒執行一次
+                timer1.Enabled = true;//打開器
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
---
+++ TipERP/AutoClosedProcess/AutoClosedProcess/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Security.Principal;
+using System.Diagnostics;
+
+
+namespace AutoClosedProcess
+{
+    class Program
+    {        
+        static void Main(string[] args) {
+            if ( args == null || args.Length < 2)
+            {
+                Console.WriteLine("please input process and program name..");
+                Console.WriteLine("Porcess：");
+                Console.WriteLine("s: start porcess.");
+                Console.WriteLine("c: close porcess.");
+                return;
+            }
+
+            Program s = new Program();
+            string sw, ProcessName, user;
+
+            
+            sw = args[0].Length > 0 ? args[0] : "";
+            user = WindowsIdentity.GetCurrent().Name;
+            ProcessName = args[1].Length > 1 ? args[1] : "";
+
+            switch (sw.ToUpper())
+            {
+                //Close the specified process.
+                case "S":
+                    s.StartProcess(ProcessName);
+                    break;
+                //Close the specified process.
+                case "C":
+                    s.CloseProcess(ProcessName);
+                    break;
+                default:
+                    s.IsProcessExist(ProcessName);
+                    break;
+            }
+        }
+
+        //啟動應用程式
+        private void StartProcess(string ProcessName)
+        {
+            try
+            {
+                Process.Start(ProcessName);
+                Console.WriteLine(Process.Start(ProcessName).Id);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        //關閉應用程式
+        private void CloseProcess(string ProcessName)
+        {
+            try
+            {
+                Process[] ps = Process.GetProcesses();
+                foreach (Process p in ps)
+                {
+                    if (p.ProcessName == ProcessName)
+                    {
+                        Console.WriteLine(p.Id);
+                        p.CloseMainWindow();
+                        p.Close();
+                        //p.Kill();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        //強制關閉應用程式
+        private void KillProcess(string ProcessName, int ProcessID)
+        {
+            try
+            {
+                Process[] ps = Process.GetProcesses();
+                foreach (Process p in ps)
+                {
+                    if (p.ProcessName == ProcessName && p.Id == ProcessID)
+                    {                       
+                        p.Kill();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        //判斷應用程式是否執行中
+        private bool IsProcessExist(string ProcessName)
+        {
+            try
+            {
+                Process[] ps = Process.GetProcesses();
+                foreach (Process p in ps)
+                {
+                    if (p.ProcessName == ProcessName)
+                    {
+                        return true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return false;
+        }
+    }
+}
